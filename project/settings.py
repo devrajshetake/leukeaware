6189e6dad6661bd75899cbbc9e124a1aa013c28b
devrajshetake <79364404+devrajshetake@users.noreply.github.com>
2021-11-20 03:34:34 +0530

    nehil

--- project/settings.py
+++ project/settings.py
@@ -126,14 +126,10 @@ USE_TZ = True
 #         os.path.join(BASE_DIR,'static'),
 #         ]
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,  'static')
-
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'static'),
-        ]
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
+
 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
